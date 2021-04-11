f453be9bd6b4e10de324be233b9e6233db2f7d00
malapashish <malapashish10@gmail.com>
2021-04-11 18:50:32 +0530

    Step 2 Completed

--- part2/data_for_countries/src/App.js
+++ part2/data_for_countries/src/App.js
@@ -6,7 +6,9 @@ import { v4 as uuidv4 } from 'uuid';
 const App = () => {
 
     const [countries , setCountries] = useState('');
-    const [countryList , setCountryList] = useState([]);
+    const [countryList , setCountryList] = useState([]);  
+    const [selectedCountry , setSelectedCountry] = useState([]);
+    const [isClicked , setIsClicked] = useState(false);
 
     useEffect(() => {
         axios
@@ -17,32 +19,33 @@ const App = () => {
     },[countries])
 
     const handleCountryName = (event) => {
+        setSelectedCountry([]); 
+        setIsClicked(false);
         setCountries(event.target.value);
     };
+ 
+
+    const renderLanguageList = (lang) => {
+            return(
+                <>
+                    <h3>Spoken Languages</h3> 
+                    <ul>
+                        {
+                            lang.map((test) => 
+                                <li key = {uuidv4()}>
+                                    {test.name}
+                                </li>
+                            )
+                        }
+                    </ul>
+                </>
+            )
+    }
+    
+
 
     const renderResult = () => {
-        if(countryList.length === 1){
-            return(
-                <ul>
-                    {
-                        countryList.map((country) => 
-                            <div>
-                                <h3>{country.name}</h3>
-                                <p>Capital : {country.capital}</p>
-                                <p>Population : {country.population}</p>
-                                <h3>Spoken Languages</h3>
-                                <ul>
-                                    {country.languages.map((language) => 
-                                        <li key = {uuidv4()}>{language.name}</li>
-                                    )}
-                                </ul>
-                                <img src = {country.flag} alt = 'Country Flag' style = {{width : '200px' , height : '100px'}} />
-                            </div>
-                        )
-                    }
-                </ul>
-            )
-        }else if(countryList.length > 10){
+        if(countryList.length > 10){
             return(
                 <h3>Too much matches , specify another filter</h3>
             )
@@ -51,18 +54,34 @@ const App = () => {
                 <ul>
                     {
                         countryList.map((country) => 
-                            <li key = {uuidv4()}>{country.name}</li>
+                            <li key = {uuidv4()}>
+                                {country.name}
+                            <button onClick = {() => {setSelectedCountry(country); setIsClicked(true);}} >
+                                Click
+                            </button>    
+                            </li>
                         )
                     }
-                </ul> 
+                </ul>
             )
         }
-    }
+    };
+ 
 
     return(
         <div>
         Find Countries <input value = {countries} onChange = {handleCountryName} />
         {renderResult()} 
+        { isClicked && (
+            <div>
+                <h3>{selectedCountry.name}</h3>
+                <p>{selectedCountry.capital}</p>
+                <p>{selectedCountry.population}</p>
+                {renderLanguageList(Object.values(selectedCountry.languages))} 
+                <img src = {selectedCountry.flag} alt = 'Country flag' style = {{width : '200px' , height : '100px' , display : 'block'}} /> 
+                <button onClick = {() => setIsClicked(false)} >Close</button>
+            </div>
+        )}
         </div>
     );
 };
