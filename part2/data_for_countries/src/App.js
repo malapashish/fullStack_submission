9d986330078465043ab4f3c477f5af0edca1b845
malapashish <malapashish10@gmail.com>
2021-04-18 17:58:55 +0530

    Weather feature added

--- part2/data_for_countries/src/App.js
+++ part2/data_for_countries/src/App.js
@@ -3,12 +3,22 @@ import React , {useState , useEffect} from 'react';
 import axios from 'axios';
 import { v4 as uuidv4 } from 'uuid';
 
+require('dotenv').config();
+
+
+  
+
 const App = () => {
 
     const [countries , setCountries] = useState('');
     const [countryList , setCountryList] = useState([]);  
     const [selectedCountry , setSelectedCountry] = useState([]);
     const [isClicked , setIsClicked] = useState(false);
+    const [capital , setCapital] = useState('');
+    const [countryWeather , setCountryWeather] = useState([]);
+    const api_key = process.env.REACT_APP_API_KEY ;
+    
+
 
     useEffect(() => {
         axios
@@ -18,8 +28,18 @@ const App = () => {
             })
     },[countries])
 
+    useEffect(() => {
+        axios
+            .get(`http://api.weatherstack.com/current?access_key=${api_key}&query=${capital}`)
+            .then((response) => {
+                setCountryWeather(response.data.current);
+                console.log(response.data.current);
+            })
+    },[capital , api_key]);
+
     const handleCountryName = (event) => {
         setSelectedCountry([]); 
+        setCountryWeather([]);
         setIsClicked(false);
         setCountries(event.target.value);
     };
@@ -56,7 +76,11 @@ const App = () => {
                         countryList.map((country) => 
                             <li key = {uuidv4()}>
                                 {country.name}
-                            <button onClick = {() => {setSelectedCountry(country); setIsClicked(true);}} >
+                            <button onClick = {() => {
+                                setSelectedCountry(country); 
+                                setIsClicked(true); 
+                                setCapital(country.capital)}} 
+                            >
                                 Click
                             </button>    
                             </li>
@@ -75,13 +99,21 @@ const App = () => {
         { isClicked && (
             <div>
                 <h3>{selectedCountry.name}</h3>
-                <p>{selectedCountry.capital}</p>
-                <p>{selectedCountry.population}</p>
+                <p>Capital : {selectedCountry.capital}</p>
+                <p>Population : {selectedCountry.population}</p>
                 {renderLanguageList(Object.values(selectedCountry.languages))} 
                 <img src = {selectedCountry.flag} alt = 'Country flag' style = {{width : '200px' , height : '100px' , display : 'block'}} /> 
+                { countryWeather && (
+                    <div>
+                        <p><b>Temperature :</b>{countryWeather.temperature} Celcius </p>
+                        <img src = {countryWeather.weather_icons} alt = {countryWeather.weather_descriptions} style = {{ width : '100px' , height : '100px' , display : 'block' }} /> 
+                        <p><b>Wind : </b>{countryWeather.wind_speed} mph direction {countryWeather.wind_dir}</p>
+                    </div>
+                )}
                 <button onClick = {() => setIsClicked(false)} >Close</button>
             </div>
-        )}
+        )} 
+        
         </div>
     );
 };
