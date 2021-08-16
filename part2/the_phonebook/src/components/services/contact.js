af1da239bbe029158f91b210f66da4306e1bd9d3
malapashish <malapashish10@gmail.com>
2021-08-16 11:10:31 +0530

    Update contact.js

--- part2/the_phonebook/src/components/services/contact.js
+++ part2/the_phonebook/src/components/services/contact.js
@@ -1,31 +1,31 @@
-import axios from 'axios';
-const baseUrl = 'http://localhost:3001/persons';
+import axios from "axios";
+const baseUrl = "http://localhost:3001/api/persons";
 
 const getContacts = () => {
-    const request = axios.get(baseUrl);
-    return request.then(response => response.data);
-}
+  const request = axios.get(baseUrl);
+  return request.then((response) => response.data);
+};
 
-const createContact = newContact => {
-    const request = axios.post(baseUrl , newContact);
-    return request.then(response => response.data);
-}
+const createContact = (newContact) => {
+  const request = axios.post(baseUrl, newContact);
+  return request.then((response) => response.data);
+};
 
-const deleteContact = id => {
-    const request = axios.delete(`${baseUrl}/${id}`);
-    return request.then(response => response.data);
-}
+const deleteContact = (id) => {
+  const request = axios.delete(`${baseUrl}/${id}`);
+  return request.then((response) => response.data);
+};
 
-const updateContact = (id , newContactObject) =>{
-    const request = axios.put(`${baseUrl}/${id}` , newContactObject);
-    return request.then(response => response.data);
-} 
+const updateContact = (id, newContactObject) => {
+  const request = axios.put(`${baseUrl}/${id}`, newContactObject);
+  return request.then((response) => response.data);
+};
 
 const contactServices = {
-    getContacts,
-    createContact ,
-    deleteContact ,
-    updateContact
-}
+  getContacts,
+  createContact,
+  deleteContact,
+  updateContact,
+};
 
 export default contactServices;
